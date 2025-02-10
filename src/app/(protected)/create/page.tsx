1a4696d144760c492bdbb090c070413577387c0c
vahghar <raghav10g@gmail.com>
2025-02-11 02:50:00 +0530

    fixed-credits

--- src/app/(protected)/create/page.tsx
+++ src/app/(protected)/create/page.tsx
@@ -3,6 +3,7 @@ import { Button } from '@/components/ui/button'
 import { Input } from '@/components/ui/input'
 import useRefetch from '@/hooks/use-refetch'
 import { api } from '@/trpc/react'
+import { Info } from 'lucide-react'
 import React from 'react'
 import {useForm} from 'react-hook-form'
 import {toast} from "react-toastify"
@@ -17,24 +18,33 @@ const CreatePage = () => {
     const {register, handleSubmit, reset} = useForm<FormInput>()
     const createProject  = api.project.createProject.useMutation()
     const refetch = useRefetch()
+    const checkCredits = api.project.checkCredits.useMutation()
 
     function onSubmit(data: FormInput) {
-        createProject.mutate({
-            githubUrl: data.repoUrl,
-            name: data.projectName,
-            githubToken: data.githubToken
-        },{
-            onSuccess: ()=>{
-                toast.success("Project created successfully")
-                refetch()
-                reset()
-            },
-            onError: (err)=>{
-                toast.error(err.message)
-            }
-        })
-        return true
+        if(!!checkCredits.data){
+            createProject.mutate({
+                githubUrl: data.repoUrl,
+                name: data.projectName,
+                githubToken: data.githubToken
+            },{
+                onSuccess: ()=>{
+                    toast.success("Project created successfully")
+                    refetch()
+                    reset()
+                },
+                onError: (err)=>{
+                    toast.error(err.message)
+                }
+            })
+        } else {
+            checkCredits.mutate({
+                githubUrl: data.repoUrl,
+                githubToken: data.githubToken
+            })
+        }
     }
+
+    const hasEnoughCredits = checkCredits?.data?.userCredits ? checkCredits.data.fileCount <= checkCredits.data.userCredits : true
 
     return (
         <div className='flex items-center gap-12 h-full justify-center'>
@@ -54,9 +64,22 @@ const CreatePage = () => {
                         <Input {...register('repoUrl',{required:true})} placeholder='Github Url' type='url' required />
                         <div className='h-2'></div>
                         <Input {...register('githubToken')} placeholder='Github Token (optional)'/>
+
+                        {!!checkCredits.data && (
+                            <>
+                                <div className="mt-4 bg-orange-50 px-4 py-2 rounded-md border border-orange-200 text-orange-600">
+                                    <div className="flex items-center gap-2">
+                                        <Info className='size-4'/>
+                                        <p className='text-sm'>You will be charged <strong>{checkCredits.data?.fileCount}</strong> credits for this repo.</p>
+                                    </div>
+                                    <p className='text-sm text-blue-600 ml-6'>You have <strong>{checkCredits.data?.userCredits}</strong> credits remaining.</p>
+                                </div>
+                            </>
+                        )}
+
                         <div className='h-4'></div>
-                        <Button type='submit' disabled={createProject.isPending}>
-                            Create Project
+                        <Button type='submit' disabled={createProject.isPending || !!checkCredits.isPending || !hasEnoughCredits}>
+                            {!!checkCredits.data ?  "Create Project": "Check Credits"}
                         </Button>
                     </form>
                 </div>
--- src/server/api/routers/project.ts
+++ src/server/api/routers/project.ts
@@ -1,7 +1,7 @@
 import { z } from "zod";
 import { createTRPCRouter, protectedProcedure } from "../trpc";
 import { pollCommits } from "@/lib/github";
-import { indexGithubRepo } from "@/lib/github-loader";
+import { checkCredits, indexGithubRepo } from "@/lib/github-loader";
 import { AlignVerticalSpaceBetween } from "lucide-react";
 
 export const projectRouter = createTRPCRouter({
@@ -12,6 +12,21 @@ export const projectRouter = createTRPCRouter({
             githubToken : z.string().optional()
         })
     ).mutation(async({ctx,input})=>{
+        const user = await ctx.db.user.findUnique({
+            where: {
+                id: ctx.user.userId || ""
+            }, select: {
+                credits: true
+            }
+        })
+        if(!user){
+            throw new Error("user not found")
+        }
+        const currentCredits = user.credits || 0;
+        const fileCount = await checkCredits(input.githubUrl,input.githubToken)
+        if(currentCredits < fileCount){
+            throw new Error("not enough credits")
+        }
         const project = await ctx.db.project.create({
             data:{
                 githubUrl: input.githubUrl,
@@ -25,6 +40,16 @@ export const projectRouter = createTRPCRouter({
         })
         await indexGithubRepo(project.id,input.githubUrl,input.githubToken)
         await pollCommits(project.id)
+        await ctx.db.user.update({
+            where: {
+                id: ctx.user.userId || ""
+            },
+            data: {
+                credits: {
+                    decrement: fileCount
+                }
+            }
+        })
         return project
     }),
 
@@ -131,4 +156,20 @@ export const projectRouter = createTRPCRouter({
             }
         })
     }),
+    checkCredits: protectedProcedure.input(z.object({
+        githubUrl: z.string(),githubToken: z.string().optional()
+    })).mutation(async ({ctx,input})=>{
+        const fileCount =  await checkCredits(input.githubUrl,input.githubToken)
+        const userCredits = await ctx.db.user.findUnique({
+            where: {
+                id: ctx.user.userId || ""
+            },
+            select: {
+                credits: true
+            }
+        })
+        return {
+            fileCount, userCredits: userCredits?.credits || 0
+        }
+    })
 })
